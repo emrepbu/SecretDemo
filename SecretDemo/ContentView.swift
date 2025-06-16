9cc9fb0aeaecb7012db3d73f8e927b54f26f3415
emrepbu <emrewindowsarg@gmail.com>
2025-06-16 18:23:18 +0300

    Update ContentView.swift

--- SecretDemo/ContentView.swift
+++ SecretDemo/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var apiKey: String {
+        Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String ?? "API Key Not Found"
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("GitHub Secrets Demo")
+                .font(.title)
+                .bold()
+            Text("API Key:")
+                .font(.headline)
+            Text(apiKey)
+                .font(.body)
+                .foregroundColor(.blue)
+                .multilineTextAlignment(.center)
+                .padding()
         }
         .padding()
     }
