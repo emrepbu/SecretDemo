b645c88f80ffb691b8221e6edefaa0520aaec537
emrepbu <emrewindowsarg@gmail.com>
2025-06-16 18:15:49 +0300

    Initial Commit

---
+++ SecretDemo/SecretDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecretDemoApp.swift
+//  SecretDemo
+//
+//  Created by emre argana on 16.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct SecretDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
